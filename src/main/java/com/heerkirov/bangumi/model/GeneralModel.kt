8728c6f1efaf438550acf7cb39edb6a40596ea28
HeerKirov <HeerKirov@outlook.com>
2018-04-15 20:01:13 +0800

    更新新的model。

--- src/main/java/com/heerkirov/bangumi/model/GeneralModel.kt
+++ src/main/java/com/heerkirov/bangumi/model/GeneralModel.kt
@@ -1,16 +1,9 @@
 package com.heerkirov.bangumi.model
 
-import com.heerkirov.bangumi.model.base.ArrayType
 import com.heerkirov.bangumi.model.base.Model
-import com.heerkirov.bangumi.model.base.ModelInterface
 import com.heerkirov.bangumi.model.base.UBModel
-import org.hibernate.annotations.Cascade
-import org.hibernate.annotations.CascadeType
-import org.hibernate.annotations.Type
-import org.hibernate.annotations.TypeDef
 import java.util.*
 import javax.persistence.*
-import kotlin.collections.ArrayList
 import kotlin.collections.HashSet
 
 @Entity @Table(name = "public.series")
@@ -59,7 +52,6 @@ class Company(
 ): UBModel()
 
 @Entity @Table(name = "public.anime")
-@TypeDef(name = "array", typeClass = ArrayType::class)
 class Anime constructor(
         @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="ANIME_ID_SEQ")
         @SequenceGenerator(name="ANIME_ID_SEQ", sequenceName="anime_id_seq", allocationSize = 1)
@@ -92,43 +84,79 @@ class Anime constructor(
         @Column(name = "level_r18g")var levelR18G: Double? = null //R18G评级
 ): UBModel()
 
+@Entity @Table(name = "public.bangumi")
 class Bangumi(
-        var id: Int,
-        var name: String,
-        var number: Int,
-        var anime_id: Int,
-        var publish_time: Date,
-        var play_type: String,
-        var play_length: Int,
-        var play_count: Int,
-        //var companies
+        @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="BANGUMI_ID_SEQ")
+        @SequenceGenerator(name="BANGUMI_ID_SEQ", sequenceName="bangumi_id_seq", allocationSize = 1)
+        @Column(name = "id")var id: Int? = null,
+        @Column(name = "uid", nullable = false)var uid: Int? = null,
+        @Column(name = "name", nullable = false, length = 128)var name: String = "",
+        @Column(name = "serial", nullable = false)var serial: Int? = null,
+        @ManyToOne@JoinColumn(name = "anime_id", nullable = false)var anime: Anime? = null,
+        @ManyToMany(targetEntity = Company::class,  fetch = FetchType.EAGER)@JoinTable(name = "public.company_to_bangumi",
+                joinColumns = [JoinColumn(name = "bangumi_id")],
+                inverseJoinColumns = [JoinColumn(name = "company_id")]) var companyList: Set<Company> = HashSet(),
 
-        var finished_time: Date,
-        var watching: Boolean,
-        var double: Boolean,
-        var origin: Boolean,
+        @Column(name = "publish_time")var publishTime: Calendar? = null,
+        @Column(name = "play_type", length = 12, nullable = false)var playType: String = "tv",
+        @Column(name = "play_length")var playLength: Int? = null,
+        @Column(name = "play_quantity")var playQuantity: Int? = null,
 
-        var user_id: String
-)
+        @Column(name = "finished_time")var finishedTime: Calendar? = null,
+        @Column(name = "watching", nullable = false)var watching: Boolean = false,
+        @Column(name = "multiple_time", nullable = false)var multipleTimes: Boolean = false,
+        @Column(name = "seen_the_original", nullable = false)var SeenTheOriginal: Boolean = false,
 
+        @Column(name = "user_id", nullable = false) var userId: String? = null,
+        @Column(name = "create_time", nullable = false)var createTime: Calendar? = null,
+        @Column(name = "update_time", nullable = false)var updateTime: Calendar? = null,
+
+        @Column(name = "score_like")var scoreLike: Double? = null, //喜好评分
+        @Column(name = "score_patient")var scorePatient: Double? = null, //耐用性评分
+
+        @Column(name = "make_make")var makeMake: Double? = null, //制作
+        @Column(name = "make_drama")var makeDrama: Double? = null, //剧本
+        @Column(name = "make_music")var makeMusic: Double? = null, //音乐
+        @Column(name = "make_person")var makePerson: Double? = null, //人物
+        @Column(name = "make_background")var makeBackground: Double? = null, //背景
+
+        @Column(name = "level_r18")var levelR18: Double? = null, //R18评级
+        @Column(name = "level_r18g")var levelR18G: Double? = null, //R18G评级
+
+        @ManyToMany(targetEntity = Tag::class,  fetch = FetchType.EAGER)@JoinTable(name = "public.tag_to_bangumi",
+                joinColumns = [JoinColumn(name = "bangumi_id")],
+                inverseJoinColumns = [JoinColumn(name = "tag_id")]) var tagList: Set<Tag> = HashSet()
+): UBModel()
+
+@Entity @Table(name = "public.episode")
 class Episode (
-        var id: Int,
-        var bangumi_id: Int,
-        var number: Int,
-        var name: String,
-        var publish_time: Date,
-        var finished_time: Date,
+        @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="EPISODE_ID_SEQ")
+        @SequenceGenerator(name="EPISODE_ID_SEQ", sequenceName="episode_id_seq", allocationSize = 1)
+        @Column(name = "id")var id: Int? = null,
+        @ManyToOne@JoinColumn(name = "bangumi_id", nullable = false)var bangumi: Bangumi? = null,
+        @Column(name = "serial", nullable = false)var serial: Int? = null,
+        @Column(name = "name", nullable = false, length = 128)var name: String = "",
+        @Column(name = "publish_time")var publishTime: Calendar? = null,
 
-        var user_id: String
-)
+        @Column(name = "finished_time")var finishedTime: Calendar? = null,
 
+        @Column(name = "user_id", nullable = false) var userId: String? = null,
+        @Column(name = "create_time", nullable = false)var createTime: Calendar? = null,
+        @Column(name = "update_time", nullable = false)var updateTime: Calendar? = null
+): UBModel()
+
+@Entity @Table(name = "public.tag")
 class Tag(
-        var id: Int,
-        var name: String,
-        var description: String,
-        var parent: Int?,
+        @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="TAG_ID_SEQ")
+        @SequenceGenerator(name="TAG_ID_SEQ", sequenceName="tag_id_seq", allocationSize = 1)
+        @Column(name = "id")var id: Int? = null,
+        @Column(name = "name", nullable = false, length = 8)var name: String = "",
+        @Column(name = "description", nullable = false, length = 128)var description: String = "",
+        @Column(name = "parent")var parent: Int? = null,
 
-        var user_id: String
+        @Column(name = "user_id", nullable = false) var userId: String? = null,
+        @Column(name = "create_time", nullable = false)var createTime: Calendar? = null,
+        @Column(name = "update_time", nullable = false)var updateTime: Calendar? = null
 )
 
 @Entity @Table(name = "public.author_to_anime")
